e456190a7e8ee702fc8cb544d4631253abadb862
suoyunqing <yunqing.suo@thoughtworks.com>
2023-09-08 19:24:26 +0800

    init repo

---
+++ src/test/java/com/xuezhidao/teacherservice/TeacherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuezhidao.teacherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
